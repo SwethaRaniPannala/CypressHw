5a8d362ec4a1ed06e3bfaff20e31437450f4a602
Swetha Rani Pannala <swetharani.pannala@gmail.com>
2024-06-18 13:37:08 +0200

    html report config

--- CypressHomeProject/cypress.config.js
+++ CypressHomeProject/cypress.config.js
@@ -12,9 +12,19 @@ module.exports = defineConfig({
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    videoOnFailOnly:true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
